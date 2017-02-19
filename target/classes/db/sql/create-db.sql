6a671c2f77ae4604cc5a90c7ad3b8a2f618a501a
bhalchandraambre <bhalchandraambre@gmail.com>
2017-02-19 06:15:20 +0530

    Removing generated dir

--- target/classes/db/sql/create-db.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE student (
-  id         INTEGER PRIMARY KEY,
-  name VARCHAR(30),
-  address  VARCHAR(50)
-);
--- target/classes/db/sql/insert-db.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO student VALUES (1, 'mkyong', 'mkyong@gmail.com');
-INSERT INTO student VALUES (2, 'alex', 'alex@yahoo.com');
-INSERT INTO student VALUES (3, 'joel', 'joel@gmail.com');
